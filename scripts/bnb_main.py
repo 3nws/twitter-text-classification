ca37dcca7dee987fb11109bdad130205d5dfe3e4
Enes <eneskurbetoglu1@gmail.com>
2022-03-24 21:42:49 +0300

    bnb, xgboost, and lstm

--- scripts/bnb_main.py
+++ scripts/bnb_main.py
@@ -1,60 +1,62 @@
-from nltk.tokenize import TweetTokenizer, RegexpTokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import RegexpTokenizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 import re
 import pandas as pd
 import pickle
+import nltk
+import preprocessor as p
 
-# importing the dataset
-# DATASET_COLUMNS  = ["sentiment", "ids", "date", "flag", "user", "tweet"]
-# DATASET_ENCODING = "ISO-8859-1"
-# dataset = pd.read_csv('./covid4.csv', delimiter=',', encoding=DATASET_ENCODING , names=DATASET_COLUMNS)
+dataset_dir = "imdb"
+# dataset_dir = "sentiment140"
+# dataset_dir = "coronaNLP"
 
-DATASET_ENCODING = "ISO-8859-1"
-# dataset = pd.read_csv('./training.1600000.processed.noemoticon.csv', delimiter=',', encoding=DATASET_ENCODING , names=DATASET_COLUMNS)
+# importing the processed dataframe
+df = joblib.load(f'../dataframes/df_{dataset_dir}.pkl')
 
-# dataset = pd.read_csv('./Corona_NLP_train.csv', delimiter=',', encoding=DATASET_ENCODING)
-dataset = pd.read_csv('./IMDB Dataset.csv', delimiter=',', encoding=DATASET_ENCODING)
 
-dataset = dataset.drop_duplicates()
+X = df.iloc[:, 0]
 
-# removing the unnecessary columns.
-# dataset = dataset[['sentiment','tweet']]
+y = df.iloc[:, 1]
 
-token = RegexpTokenizer(r'[a-zA-Z0-9]+')
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
-tfidf = TfidfVectorizer(stop_words='english', max_features=20000, ngram_range=(1,2), tokenizer=token.tokenize)
+# creating our pipeline that will return an estimator
+pipeline = Pipeline([('clf', BernoulliNB())])
 
-X = dataset['review']
+parameters = {
+    'clf__fit_prior': (False, True),
+    'binarize': (0.25, 0.5, 1.0),
+    'clf__alpha': (1, 0.1, 0.01, 0.001)
+}
 
-X = tfidf.fit_transform(X)
+clf = GridSearchCV(pipeline, param_grid=parameters, cv=5, verbose=1)
 
-y = dataset['sentiment']
+clf.fit(X_train, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=15)
-
-# X_train.shape, X_test.shape
-
-# creating the model usin bernoulli naive bayes algorithm
-BNB = BernoulliNB()
-
-# training the model
-BNB.fit(X_train, y_train)
-
-# testing our predictions
-y_pred = BNB.predict(X_test)
+y_pred = clf.predict(X_test)
 
 print(classification_report(y_test, y_pred))
 
-# test_tweet = "scandinavia #news:  norway : it's illegal for employers to require covid  passports  denmark\
-#     sweden : they won't be bringing in covid  vaccination passports  #holdtheline #enoughisenough #nomedicalapartheid #nomasks #nomorelockdowns #openforall #corona #coronavirus"
-# vector = tfidf.transform([test_tweet])
+print(confusion_matrix(y_test, y_pred))
 
-# print(BNB.predict(vector))
+print("Best: %f using %s" % (clf.best_score_,
+                             clf.best_params_))
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+params = clf.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
 
-# exporting the model and the trained vectorizer
-pickle.dump(BNB, open('./models/BNB_model', 'wb'))
-pickle.dump(tfidf, open('./vector/tfidf_vectorizer', 'wb'))
+
+acc = int(accuracy_score(y_test, y_pred)*100)
+
+# exporting the pipeline
+pickle.dump(clf.best_estimator_, open(f'../bnb_{dataset_dir}_{acc}', 'wb'))
